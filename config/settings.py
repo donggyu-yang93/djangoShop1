e44d48a51613fca3bd37a09d76ce7b6a13ee45aa
Mr.Yang <ydg0614@gmail.com>
2024-03-15 05:14:28 +0900

    240315 static

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/home/ubuntu/djangoShop1/static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')   #기본 경로 설정
