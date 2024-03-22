aa3f25aa1bd36674aabf9dd612310f26ce2ce18e
Mr.Yang <ydg0614@gmail.com>
2024-03-23 03:52:02 +0900

    240323 회원관리 뷰

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', register, name='register'),
-    path('update/', update, name='update'),
-    path('password/', change_password, name='change_password'),
-    path('delete/', delete, name='delete'),
 ]
 
